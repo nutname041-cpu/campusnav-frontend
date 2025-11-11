346241f08e0048c595afeafee8a3440f932d718b
nutname041-cpu <nutname041@gmail.com>
2025-11-12 00:10:24 +0530

    Delete src/components/Skeleton.tsx

--- src/components/Skeleton.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Skeleton({ className = "" }: { className?: string }) {
-  return <div className={"animate-pulse bg-gray-200 dark:bg-gray-700 rounded " + className} />;
-}
